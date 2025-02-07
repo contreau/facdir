8c690f3501fe24b7880836ba97a5fb88e59afe6f
contreau <72581161+contreau@users.noreply.github.com>
2025-02-07 14:10:30 -0500

    put processing / file write scripts into functions

--- src/process_input/process.ts
+++ src/process_input/process.ts
@@ -10,7 +10,6 @@ import { checkDirectory, formatLongNames } from "./helpers";
 // Any multi-part or long names that need to be manually edited (long_names)
 // Missing profiles that can be deleted from the original HTML
 
-// * SCRIPT START
 // * Gather all html file names into one array
 const html_fileNames: string[] = [];
 const html_dir = await readdir("html/input");
@@ -23,110 +22,126 @@ const all_profiles: Array<facultyCollection> = [];
 const long_names: namesByDepartment = {};
 const missing_profiles: namesByDepartment = {};
 
-// * BEGIN FILE PROCESSING
-console.clear();
-for (let filename of html_fileNames) {
-  const facultyCollection: facultyCollection = {
-    department: "",
-    profiles: [],
-  };
-  const file = Bun.file(`html/input/${filename}`);
-  console.log(`Processing ${filename}...`);
-  const file_text = await file.text();
-  const department = filename.split(".html")[0];
+// * FUNCTIONS
+async function process_inputHTML(directory: string[]) {
+  // * BEGIN FILE PROCESSING
+  console.clear();
+  for (let filename of directory) {
+    const facultyCollection: facultyCollection = {
+      department: "",
+      profiles: [],
+    };
+    const file = Bun.file(`html/input/${filename}`);
+    console.log(`Processing ${filename}...`);
+    const file_text = await file.text();
+    const department = filename.split(".html")[0];
 
-  // * Initialize arrays in long_names and missing_profiles directories
-  if (!long_names[`${department}`]) {
-    long_names[`${department}`] = [];
-  }
-  if (!missing_profiles[`${department}`]) {
-    missing_profiles[`${department}`] = [];
-  }
-  facultyCollection.department = department;
-
-  // * Create the 'DOM' for the given html file
-  const window = new Window();
-  const document = window.document;
-  document.body.innerHTML = file_text;
-
-  // * Loop through the <a> tags to process names
-  const name_anchors = Array.from(document.querySelectorAll("p.mdbluetext a"));
-  const names: nameFields[] = [];
-  for (const a of name_anchors) {
-    const anchor = a as unknown as HTMLAnchorElement;
-    const inDirectory: boolean = await checkDirectory(anchor.href);
-    if (!inDirectory) {
-      missing_profiles[`${department}`].push(anchor.textContent);
-      continue;
+    // * Initialize arrays in long_names and missing_profiles directories
+    if (!long_names[`${department}`]) {
+      long_names[`${department}`] = [];
     }
-    let fields: nameFields;
-    let has_titles: boolean = false;
-    let stripped_name: string | null = null;
-    let split_name: string[];
-
-    // * Handle names with titles
-    if (anchor.textContent.includes(",")) {
-      has_titles = true;
-      const titled_name = anchor.textContent.split(",");
-      stripped_name = titled_name[0];
-      split_name = stripped_name.split(" ");
-    } else {
-      split_name = anchor.textContent.split(" ");
+    if (!missing_profiles[`${department}`]) {
+      missing_profiles[`${department}`] = [];
     }
+    facultyCollection.department = department;
 
-    // * Handle long names that had titles
-    if (split_name.length > 2 && has_titles) {
-      long_names[`${department}`].push(stripped_name as string);
-      const last_name_split = split_name.slice(1, split_name.length);
-      const lastName = last_name_split.join(" ");
-      fields = {
-        FirstName: split_name[0],
-        LastName: lastName,
-      };
-    }
+    // * Create the 'DOM' for the given html file
+    const window = new Window();
+    const document = window.document;
+    document.body.innerHTML = file_text;
 
-    // * Handle long names without titles
-    else if (split_name.length > 2 && !has_titles) {
-      // * Remove any ending parenthesized substrings, then continue
-      const last_substring = split_name.at(-1);
-      if (last_substring !== undefined && last_substring[0] === "(") {
-        split_name.pop();
-        if (split_name.length > 2) {
-          fields = formatLongNames(long_names, department, split_name, anchor);
-        } else {
-          fields = {
-            FirstName: split_name[0],
-            LastName: split_name[1],
-          };
-        }
-      } else {
-        fields = formatLongNames(long_names, department, split_name, anchor);
+    // * Loop through the <a> tags to process names
+    const name_anchors = Array.from(
+      document.querySelectorAll("p.mdbluetext a")
+    );
+    const names: nameFields[] = [];
+    for (const a of name_anchors) {
+      const anchor = a as unknown as HTMLAnchorElement;
+      const inDirectory: boolean = await checkDirectory(anchor.href);
+      if (!inDirectory) {
+        missing_profiles[`${department}`].push(anchor.textContent);
+        continue;
       }
+      let fields: nameFields;
+      let has_titles: boolean = false;
+      let stripped_name: string | null = null;
+      let split_name: string[];
+
+      // * Handle names with titles
+      if (anchor.textContent.includes(",")) {
+        has_titles = true;
+        const titled_name = anchor.textContent.split(",");
+        stripped_name = titled_name[0];
+        split_name = stripped_name.split(" ");
+      } else {
+        split_name = anchor.textContent.split(" ");
+      }
+
+      // * Handle long names that had titles
+      if (split_name.length > 2 && has_titles) {
+        long_names[`${department}`].push(stripped_name as string);
+        const last_name_split = split_name.slice(1, split_name.length);
+        const lastName = last_name_split.join(" ");
+        fields = {
+          FirstName: split_name[0],
+          LastName: lastName,
+        };
+      }
+
+      // * Handle long names without titles
+      else if (split_name.length > 2 && !has_titles) {
+        // * Remove any ending parenthesized substrings, then continue
+        const last_substring = split_name.at(-1);
+        if (last_substring !== undefined && last_substring[0] === "(") {
+          split_name.pop();
+          if (split_name.length > 2) {
+            fields = formatLongNames(
+              long_names,
+              department,
+              split_name,
+              anchor
+            );
+          } else {
+            fields = {
+              FirstName: split_name[0],
+              LastName: split_name[1],
+            };
+          }
+        } else {
+          fields = formatLongNames(long_names, department, split_name, anchor);
+        }
+      }
+
+      // * Default handling of names
+      else {
+        fields = {
+          FirstName: split_name[0],
+          LastName: split_name[1],
+        };
+      }
+      names.push(fields);
     }
 
-    // * Default handling of names
-    else {
-      fields = {
-        FirstName: split_name[0],
-        LastName: split_name[1],
-      };
+    for (let name of names) {
+      facultyCollection.profiles.push(name);
     }
-    names.push(fields);
+    all_profiles.push(facultyCollection);
+    await window.happyDOM.close();
   }
-
-  for (let name of names) {
-    facultyCollection.profiles.push(name);
-  }
-  all_profiles.push(facultyCollection);
-  await window.happyDOM.close();
 }
 
-// * Write to the output JSON files
-Bun.write("all_profiles/profiles.json", JSON.stringify(all_profiles));
-Bun.write("long_names/longnames.json", JSON.stringify(long_names));
-Bun.write(
-  "missing_profiles/missingprofiles.json",
-  JSON.stringify(missing_profiles)
-);
-console.clear();
-console.log("Finished processing.");
+function write_json() {
+  // * Write to the output JSON files
+  Bun.write("all_profiles/profiles.json", JSON.stringify(all_profiles));
+  Bun.write("long_names/longnames.json", JSON.stringify(long_names));
+  Bun.write(
+    "missing_profiles/missingprofiles.json",
+    JSON.stringify(missing_profiles)
+  );
+  console.clear();
+  console.log("Finished processing.");
+}
+
+// * SCRIPT START
+await process_inputHTML(html_fileNames);
+write_json();
